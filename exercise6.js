c4d3a4130c17e53d66587de7cf2a896893d86711
kshitij zutshi <kshitij.zutshi@gmail.com>
2020-06-22 18:22:56 +0530

    array helper reduce solved

---
+++ exercise6.js
@@ -0,0 +1,24 @@
+function countWords(inputWords) {
+    // SOLUTION GOES HERE
+    let res = inputWords.reduce(function(accumulator,currentValue) {
+        accumulator[currentValue] ? accumulator[currentValue]++ : accumulator[currentValue] = 1;
+        return accumulator;
+    }, {})
+    return res;
+  }
+  
+  module.exports = countWords
+
+/*
+
+Alternate solution
+
+  function countWords(arr) {
+    return arr.reduce(function(countMap, word) {
+      countMap[word] = ++countMap[word] || 1 // increment or initialize to 1
+      return countMap
+    }, {}) // second argument to reduce initialises countMap to {}
+  }
+  
+  module.exports = countWords
+  */
