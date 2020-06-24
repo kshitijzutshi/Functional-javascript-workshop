9de502b998c5344fe8afd930a61f8d1d26ec2f76
kshitij zutshi <kshitij.zutshi@gmail.com>
2020-06-24 12:21:59 +0530

    completed challenge

---
+++ exercise9.js
@@ -0,0 +1,11 @@
+var slice = Array.prototype.slice
+    
+function logger(namespace) {
+  // SOLUTION GOES HERE
+  return function(){
+      console.log.apply(console, [namespace].concat(slice.call(arguments)))
+  }
+
+}
+
+module.exports = logger
