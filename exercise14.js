1d9edb573408152d5ed86b22e90cb32b55052855
kshitij zutshi <kshitij.zutshi@gmail.com>
2020-06-25 17:22:32 +0530

    trampoline exe 14 complete

--- exercise14.js
+++ exercise14.js
@@ -1,16 +1,29 @@
 function repeat(operation, num) {
     // Modify this so it doesn't cause a stack overflow!
     if (num <= 0) return
-    operation()
-    return repeat(operation, --num)
+
+    // To modify repeat so that it returns the 'next step' if there is one!
+    return function(){
+        operation()
+        return repeat(operation, --num)
+    }
+    
   }
   
   function trampoline(fn) {
     // You probably want to implement a trampoline!
+    // Add a condition that this trampoline function executes sync. ly and gets steps till there are no more!!
+    while(fn && typeof fn === 'function'){
+        fn = fn();
+    }
+    return fn;
   }
   
   module.exports = function(operation, num) {
     // You probably want to call your trampoline here!
-    return repeat(operation, num)
+    return trampoline(function(){
+        return repeat(operation, num)
+    })
+   
   }
 
